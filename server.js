de2874104e9319760df18c629adcb6aeb14395d9
Paweł <p.grabczak@cognitionhub.com>
2024-12-19 15:21:56 +0100

    Moduł 26.4 done

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
 const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  fileFilter: (req, file, cb) => {
+    const allowedExtensions = ['.png', '.jpg', '.jpeg', '.gif'];
+    const fileExtension = path.extname(file.originalname).toLowerCase();
+    if (allowedExtensions.includes(fileExtension)) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  },
+});
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/contact/send-message', upload.single('projectFile'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+  const file = req.file;
+
+  if (author && sender && title && message && file) {
+    res.render('contact', { isSent: true, fileName: file.originalname });
+  } else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.get('/hello/:name', (req, res) => {
